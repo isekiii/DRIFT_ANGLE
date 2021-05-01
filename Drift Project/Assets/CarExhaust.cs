2e7f51dab70dc2564a7dd01139adedac6e7d4c07
Benas Steponavicius <steponaviciusbenas@gmail.com>
2021-05-01 13:46:41 +0300

    Added smoke particles to vehicles exhaust systems

---
+++ Drift Project/Assets/CarExhaust.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VehiclePhysics;
+
+public class CarExhaust : MonoBehaviour
+{
+    
+     float engineRevs;
+    public float exhaustRate;
+    public Rigidbody car;
+
+    public ParticleSystem exhaust;
+
+
+    void Start () {
+      //  exhaust = GetComponent<ParticleSystem>();
+    }
+    
+
+    void Update () {
+        engineRevs = 1 + car.velocity.magnitude * 3.6f;
+        
+        exhaust.emissionRate = engineRevs * exhaustRate   ;
+       Debug.Log(exhaust.emissionRate);
+    }
+    
+}
